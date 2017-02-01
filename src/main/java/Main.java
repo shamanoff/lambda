58085d0383c322bbb309eda9512348ce6e3609ba
shamanoff <shamanoff@gmail.com>
2017-02-01 15:45:24 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.Character.isDigit;
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+/*        String words[] = {"Asterix", "Omega", "Djon", "Space"};
+
+        System.out.println(Arrays.toString(words));
+
+        List<String> wariors = Arrays.asList(words);
+        wariors.stream()
+                .map(String::toLowerCase)
+                .filter("asterix"::equals)
+                .collect(Collectors.toList());
+
+        System.out.println("Wariors: "+ wariors.toString());*/
+
+        List<String> beginningWithNumbers = Stream.of("a", "labc", "abcl", "1eee")
+                .filter(value -> isDigit(value.charAt(0)))
+                .collect(toList());
+
+        System.out.println(beginningWithNumbers.toString());
+    }
+}
