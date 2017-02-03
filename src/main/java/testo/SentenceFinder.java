759db49f82b7e7b853c9e0497a816948ce99ebc7
Michael <shamanoff@gmail.com>
2017-02-03 20:59:09 +0200

    home

--- src/main/java/testo/SentenceFinder.java
+++ src/main/java/testo/SentenceFinder.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Testo {
+public class SentenceFinder {
 
 
 
--- src/main/java/testo/t2.java
+++ src/main/java/testo/t2.java
@@ -8,39 +8,52 @@ import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class t2 {
 
-    public static ArrayList<String> find(String e) {
-        ArrayList<String> ready = new ArrayList<>();
+// BreakeIterator with Locale return list of strings there every string is sentence.
+
+    private static ArrayList<String> sentenceFinder(String inputString) {
+        ArrayList<String> readyList = new ArrayList<>();
         BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
-//        String source = "This is a test. This is a T.L.A. test. Now with a Dr. in it.";
-        iterator.setText(e);
+        iterator.setText(inputString);
         int start = iterator.first();
         for (int end = iterator.next();
              end != BreakIterator.DONE;
              start = end, end = iterator.next()) {
-            ready.add(e.substring(start, end));
-//            System.out.println(content.substring(start,end));
+            readyList.add(inputString.substring(start, end));
         }
-        return (ready);
+        return (readyList);
+    }
+
+// WordComparator return true only if sentence contains full word that we are looking for
+
+    private static boolean wordComparator(String stringFromText, String targetWord){
+        String[] words = stringFromText.split("\\s+");
+        String test = "";
+        for (String s: words) {
+            if(targetWord.equalsIgnoreCase(s)){
+                test = s;
+            }
+        }
+        return !Objects.equals(test, "");
     }
 
     public static void main(String[] args) throws IOException {
 
-
         Path path = Paths.get("files/TomSawyer.txt");
         String content = new String(Files.readAllBytes(path));
-        ArrayList<String> readyS = find(content);
-        String word = "saw";
-        List<String> total = readyS.stream()
+        ArrayList<String> allStrings = sentenceFinder(content);
 
-                .map(String::toLowerCase)
-                .filter(s -> s.contains(word))
+        String targetWord = "saw"; //The word that we looking for
+
+        List<String> resultSentences = allStrings.stream()
+                .filter(s -> wordComparator(s, targetWord))
                 .collect(Collectors.toList());
 
-        for (String e: total) {
+        for (String e: resultSentences) {
             System.out.println(e);
             System.out.println();
 
